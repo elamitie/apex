6cfb30667d16a6001f2fba1c8ea08618eefee8ed
Ethan <elamitie@gmail.com>
2017-02-13 13:31:23 -0500

    Stuff and things

--- Renderer/src/graphics/mesh/mesh.h
+++ Renderer/src/graphics/mesh/mesh.h
@@ -1,36 +1,36 @@
 #pragma once
 
 #include <GL/glew.h>
-#include <glm/glm.hpp>
 #include <vector>
+
+#include "../../utils/rendering_types.h"
+#include "../assets/shader.h"
+#include "../assets/texture.h"
+#include "vertex.h"
 
 // Ethan's Note: Eventually will support model loading w/ assimp.
 // Models would extend (or own) this class. Primitives will inherit
 // from this class.
 
-// TODO: Abstract concept of a vertex would avoid testy stuff like this
-typedef std::vector<glm::vec2> Vec2Array;
-typedef std::vector<glm::vec3> Vec3Array;
-typedef std::vector<unsigned int> UIntArray;
-
-enum MeshTopology
-{
-	POINTS,
-	LINES,
-	LINE_STRIP,
-	TRIANGLES,
-	TRIANGLE_STRIP,
-	TRIANGLE_FAN
-};
-
 class Mesh
 {
 public:
-	Mesh(Vec3Array& positions, UIntArray& indices);
-	Mesh(Vec3Array& positions, Vec2Array& uv, UIntArray& indices);
-	Mesh(Vec3Array& positions, Vec2Array& uv, Vec3Array& normals, UIntArray& indices);
-	Mesh(Vec3Array& positions, Vec2Array& uv, Vec3Array& normals, Vec3Array& tangents, Vec3Array& bitangents, UIntArray& indices);
-	
-public:
+	Mesh(std::vector<Vertex>& vertices, std::vector<unsigned int>& indices, std::vector<TexturePtr>& textures);
+
+	inline std::vector<TexturePtr> GetTextures() { return mTextures; }
+	inline std::vector<Vertex> GetVertices() { return mVertices; }
+	inline std::vector<unsigned int> GetIndices() { return mIndices; }
+
+	// Later this should definitely be part of a renderer, not here.
+	void Draw(ShaderPtr shader);
+
 private:
+	void ConstructMesh();
+
+private:
+	std::vector<TexturePtr> mTextures;
+	std::vector<Vertex> mVertices;
+	std::vector<unsigned int> mIndices;
+
+	GLuint mVao, mVbo, mIbo;
 };
