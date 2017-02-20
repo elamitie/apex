729492a87401102253a12aebf705daa11567c497
Ethan <elamitie@gmail.com>
2017-02-20 13:30:56 -0500

    Removed jenky sphere and started working on basic lighting

--- demo/app/application.cpp
+++ demo/app/application.cpp
@@ -4,39 +4,46 @@
 void Application::Init()
 {
 	mCamera = new Camera({ 0.0f, 0.0f, 3.0f });
-	mShader = new Shader("../resources/shaders/basic_texture.vert", "../resources/shaders/basic_texture.frag");
+	/*mShader = new Shader("../resources/shaders/basic_texture.vert", "../resources/shaders/basic_texture.frag");
 	mShader->AddAttribute("position");
 	mShader->AddAttribute("color");
-	mShader->AddAttribute("texCoord");
+	mShader->AddAttribute("texCoord");*/
+	mLightingShader = new Shader("../resources/shaders/basic_lighting.vert", "../resources/shaders/basic_lighting.frag");
+	mLightingShader->AddAttribute("position");
+	mLightingShader->AddAttribute("normal");
+	mLampShader = new Shader("../resources/shaders/lamp.vert", "../resources/shaders/lamp.frag");
+	mLampShader->AddAttribute("position");
 
 	mTexture = new Texture2D();
 	mTexture->Load("../resources/textures/bricks.jpg");
 
-	mSphere = new Sphere(32, 32);
-	mSphere->SetTextures({ mTexture });
-
 	SetClearColor({ 114, 144, 154 });
 
-	//glGenVertexArrays(1, &mVao);
-	//glGenBuffers(1, &mVbo);
-	//glGenBuffers(1, &mIbo);
+	glGenVertexArrays(1, &mVao);
+	glGenBuffers(1, &mVbo);
 
-	//glBindVertexArray(mVao);
+	glBindBuffer(GL_ARRAY_BUFFER, mVbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	//glBindBuffer(GL_ARRAY_BUFFER, mVbo);
-	//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindVertexArray(mVao);
+	// Position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	// Normal attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+	glBindVertexArray(0);
 
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mIbo);
-	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	//// Position attribute
-	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
-	//glEnableVertexAttribArray(0);
-	//// TexCoord attribute
-	//glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-	//glEnableVertexAttribArray(2);
-
-	//glBindVertexArray(0); // Unbind VAO
+	// Then, we set the light's VAO (VBO stays the same. After all, the vertices are the same for the light object (also a 3D cube))
+	GLuint lightVAO;
+	glGenVertexArrays(1, &mLightVao);
+	glBindVertexArray(mLightVao);
+	// We only need to bind to the VBO (to link it with glVertexAttribPointer), no need to fill it; the VBO's data already contains all we need.
+	glBindBuffer(GL_ARRAY_BUFFER, mVbo);
+	// Set the vertex attributes (only position data for the lamp))
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0); // Note that we skip over the normal vectors
+	glEnableVertexAttribArray(0);
+	glBindVertexArray(0);
 }
 
 void Application::Update(float dt)
@@ -53,36 +60,36 @@ void Application::Update(float dt)
 
 void Application::Render(float dt)
 {
-	mShader->Bind();
-	//mTexture->Bind();
+	////mShader->Bind();
+	////mTexture->Bind();
 
-	//mShader->SetUniform("ourTexture", 0);
-	
-	glm::mat4 model;
-	//model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-	//model = glm::rotate(model, (GLfloat)glm::radians(glfwGetTime() * 50.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-	glm::mat4 view;
-	view = mCamera->GetView();
-	glm::mat4 projection;
-	projection = projection = glm::perspective(mCamera->Properties.Zoom, (GLfloat)GetWidth() / (GLfloat)GetHeight(), 0.1f, 100.0f);
+	////mShader->SetUniform("ourTexture", 0);
+	//
+	//glm::mat4 model;
+	////model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	////model = glm::rotate(model, (GLfloat)glm::radians(glfwGetTime() * 50.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+	//glm::mat4 view;
+	//view = mCamera->GetView();
+	//glm::mat4 projection;
+	//projection = projection = glm::perspective(mCamera->Properties.Zoom, (GLfloat)GetWidth() / (GLfloat)GetHeight(), 0.1f, 100.0f);
 
-	mShader->SetUniform("model", model);
-	mShader->SetUniform("view", view);
-	mShader->SetUniform("projection", projection);
+	///*mShader->SetUniform("model", model);
+	//mShader->SetUniform("view", view);
+	//mShader->SetUniform("projection", projection);*/
 
-	mSphere->Draw(mShader);
+	///*glBindVertexArray(mVao);
+	//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	//glBindVertexArray(0);*/
 
-	/*glBindVertexArray(mVao);
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-	glBindVertexArray(0);*/
+	////mTexture->Unbind();
+	////mShader->Unbind();
 
-	//mTexture->Unbind();
-	mShader->Unbind();
+	mLightingShader->Bind();
 }
 
 void Application::Cleanup()
 {
 	delete mTexture;
-	delete mShader;
+	//delete mShader;
 	delete mCamera;
 }
